4b8c2857cc2b7e2040bece97548cd79129bcb00d
ahmed9194 <ahmedheshamm68@gmail.com>
2025-12-18 22:03:59 +0200

    Integrate notifications for received data
    Added desktop notification for received messages.

--- tcp_client.py
+++ tcp_client.py
@@ -1,5 +1,6 @@
 import socket
 from config import *
+from plyer import notification
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,7 +9,14 @@ def start_client():
     while True:
         data = client.recv(1024)
         if data:
-            print("Notification:", data.decode())
+            message = data.decode()
+            print("Notification:", message)
+
+            notification.notify(
+                title="Email Notification",
+                message=message,
+                timeout=5
+            )
 
 if __name__ == "__main__":
     start_client()
